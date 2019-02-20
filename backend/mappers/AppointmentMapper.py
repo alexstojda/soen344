2fad208713162e994a777af53064bf97d53c4978
Philippe Lorange <philippe.lorange@gmail.com>
2019-02-20 11:19:01 -0500

    added addAppointment API endpoint

--- backend/mappers/AppointmentMapper.py
+++ backend/mappers/AppointmentMapper.py
@@ -1,5 +1,6 @@
 from backend.tdg.AppointmentTdg import AppointmentTdg
 from backend.business_objects.Appointment import Appointment
+
 class AppointmentMapper:
     def __init__(self, app):
         self.appointment_tdg = AppointmentTdg(app)
@@ -7,8 +8,11 @@ class AppointmentMapper:
     def get_appointments(self):
         return self.appointment_tdg.get_appointments()
 
-    def add_appointment(self, req):
+    def book_appointment(self, req):
         patient = req.get('patient')
         doctor = req.get('doctor')
         date = req.get('date')
-        return self.appointment_tdg.add_appointment(Appointment(patient, doctor, date))
+        if(self.appointment_tdg.get_appointment(patient, doctor, date) == None):
+            return self.appointment_tdg.add_appointment(Appointment(patient, doctor, date))
+        else:
+            return "There is already an appointment with these parameters"
