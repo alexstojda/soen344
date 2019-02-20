402ded8544d49efe2d6d0c5eaae27f4437b7ad51
Philippe Lorange <philippe.lorange@gmail.com>
2019-02-20 11:19:01 -0500

    added addAppointment API endpoint

--- backend/tdg/AppointmentTdg.py
+++ backend/tdg/AppointmentTdg.py
@@ -3,7 +3,9 @@ from flask import json, jsonify
 from flask import Flask, render_template
 from backend.business_objects.Appointment import Appointment
 
+
 class AppointmentTdg:
+
     def __init__(self, app):
         self.mysql = MySQL()
 
@@ -25,12 +27,30 @@ class AppointmentTdg:
         if(res is None):
             return False
         else:
-            return json.dumps(data)
-
+            return data
+    
+    def get_appointment(self, patient, doctor, date):
+        connection = self.mysql.connect()
+        cursor = connection.cursor()
+        res = cursor.execute("SELECT * from appointment WHERE patient = %s AND doctor = %s AND date = %s", (patient, doctor, date))
+        row_headers = [x[0] for x in cursor.description]  # this will extract row headers
+        data = []
+        for row in cursor.fetchall():
+            data.append(dict(zip(row_headers, row)))
+        cursor.close()
+        print("8=================D")
+        print(res)
+        if(res == 0):
+            return None
+        else:
+            return data
     def add_appointment(self, appointment):
         connection = self.mysql.connect()
         cursor = connection.cursor()
         cursor.execute("""INSERT INTO appointment(patient, doctor, date)
                         VALUES(%s, %s, %s)""", 
                         (appointment.patient, appointment.doctor, appointment.date))
-        return jsonify(connection.commit())
+        cursor.execute("SELECT * FROM appointment ORDER BY id DESC")
+        result = cursor.fetchone()
+        connection.commit()
+        return jsonify(result)
