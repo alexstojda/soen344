d7c1ecc7bf2de0ca04b51096aec9869b1cd90d8a
Andrew Hanichkovsky <andrew@hanichkovsky.me>
2019-03-02 12:52:51 -0500

    dropping flask

--- backend/mappers/UserMapper.py
+++
@@ -1,8 +0,0 @@
-from backend.tdg.UserTdg import UserTdg
-
-class UserMapper:
-    def __init__(self, app):
-        self.user_tdg = UserTdg(app)
-
-    def get_doctors(self):
-        return self.user_tdg.get_doctors()
