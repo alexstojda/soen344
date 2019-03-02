9b9787299140fa423cc46ff06a542873a5fdc53e
Andrew Hanichkovsky <andrew@hanichkovsky.me>
2019-03-02 14:30:43 -0500

    added back eslint and added tailwind.css for faster ui building

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-tailwind');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +14,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .tailwind();
+
+
+if (mix.inProduction()) {
+    mix.sourceMaps().version();
+}
