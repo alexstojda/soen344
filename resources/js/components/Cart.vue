95974cdac9eb94f8fbb67bceb2573d2a23229d41
AjmerSingh7537 <a_gadreh@encs.concordia.ca>
2019-03-07 19:37:53 -0500

    Minor: changed the checkout button in the modal

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -1,28 +1,31 @@
 <template>
     <div>
-
         <button type="button" class="btn" data-toggle="modal" data-target="#modal">
             <i class="fa fa-shopping-cart"
                style="font-size:20px;color:grey;"></i>
         </button>
 
-        <div class="modal" id="modal">
+        <!-- Modal -->
+        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLongTitle">Your Cart</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        </button>
                     </div>
-
                     <div class="modal-body">
-                        <div v-bind:class="cart" v-for="appointment in cart">
+                        <div v-bind:class="cart" v-for="appointment in cart" :key="appointment.id">
                             Appointment with {{appointment.doctor}} on {{appointment.date}}
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Remove</button>
                             <br/>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <a href="/api/checkout" class="btn btn-info" role="button">Checkout</a>
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <form action="/api/checkout" method="get">
+                            <button type="submit" class="btn btn-success">Checkout</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -58,24 +61,3 @@
         }
     }
 </script>
-
-<style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-    a {
-        color: #42b983;
-    }
-    textarea {
-        width: 600px;
-        height: 200px;
-    }
-</style>
