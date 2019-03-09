d9d56a9bb7ce142cf3ffb406e24694d11f7eec0a
NicoChampagne <nicolas.b.champagne@gmail.com>
2019-03-09 14:30:58 -0500

    Added timepicker to addAvailability and setTime function, to pass proper dateTimes. + Changed ViewAppointments date format

--- resources/js/components/AddAvailability.vue
+++ resources/js/components/AddAvailability.vue
@@ -15,13 +15,13 @@
         <div class="form-group row">
             <label class="col-sm-3 offset-md-1 col-md-3 col-lg-2 col-form-label">Start Time</label>
             <div class="col-sm-9 col-md-7 col-lg-8">
-                <input v-model="startTime" type="text" class="form-control" placeholder="Start Time">
+                <vue-timepicker v-model="startTime"></vue-timepicker>
             </div>
         </div>
         <div class="form-group row">
             <label class="col-sm-3 offset-md-1 col-md-3 col-lg-2 col-form-label">End Time</label>
             <div class="col-sm-9 col-md-7 col-lg-8">
-                <input v-model="endTime" type="text" class="form-control" placeholder="End Time">
+                <vue-timepicker v-model="endTime"></vue-timepicker>
             </div>
         </div>
         <div class="form-group row">
@@ -35,18 +35,26 @@
 <script>
     import axios from "axios";
     import Datepicker from 'vuejs-datepicker';
+    import VueTimepicker from 'vuejs-timepicker';
     export default {
         name: "AddAvailability",
         data () {
             return {
                 doctor_id: "",
                 date: "",
-                startTime: "",
-                endTime: ""
+                startTime: {
+                    HH: "",
+                    mm: "",
+                },
+                endTime: {
+                    HH: "",
+                    mm: "",
+                }
             }
         },
         components: {
-            Datepicker
+            Datepicker,
+            VueTimepicker
         },
         mounted() {
         },
@@ -54,8 +62,8 @@
             addAvailability: function() {
                 axios.post('api/addAvailability', {
                     doctor_id: this.doctor_id,
-                    start: this.date + this.startTime,
-                    end: this.date + this.endTime
+                    start: this.setDate(this.date, this.startTime),
+                    end: this.setDate(this.date, this.endTime)
                 }).then(response => {
                     if(response.status == 200 || response.status == 201) {
                         console.log("Added availability")
@@ -64,6 +72,12 @@
                         console.log("Add availability failed: Response code " + response.status)
                     }
                 })
+            },
+            setDate: function(date, time) {
+                let dateMaker = new Date(date);
+                    dateMaker.setHours(time.HH);
+                    dateMaker.setMinutes(time.MM);
+                return date;
             }
         }
     };
