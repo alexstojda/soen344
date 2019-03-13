0123e1b8918f95972db91fc449080963656e1200
NicoChampagne <nicolas.b.champagne@gmail.com>
2019-03-13 18:39:26 -0400

    Added hours and minutes to view appointments page

--- resources/js/components/ViewAppointments.vue
+++ resources/js/components/ViewAppointments.vue
@@ -73,7 +73,7 @@
                 })
             },
             dateFormatter: function(date) {
-                return moment(date).format("YYYY-MM-DD");
+                return moment(date).format("YYYY-MM-DD HH:mm");
             }
         }
     };
